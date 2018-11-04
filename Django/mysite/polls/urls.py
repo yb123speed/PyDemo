631c4af337017a67b1ab7d7af144e8a48f233142
yb123speed <904896209@qq.com>
2018-11-04 23:35:33 +0800

    添å删除硬编码

--- Django/mysite/polls/urls.py
+++ Django/mysite/polls/urls.py
@@ -10,7 +10,8 @@ urlpatterns = [
     url('index2/', views.index2, name= 'index2'),
     # ex: /polls/5/
     url(r'^detail2/(?P<question_id>[0-9]+)/$', views.detail2, name='detail2'),
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    path('<int:question_id>/', views.detail, name='detail'),
     # ex: /polls/5/results/
     url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
     # ex: /polls/5/vote/
