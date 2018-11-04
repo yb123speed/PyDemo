3f436ab72d7493fbadc599f9e22adc5511c61411
yb123speed <904896209@qq.com>
2018-11-04 23:07:01 +0800

    引入django template

--- Django/mysite/polls/urls.py
+++ Django/mysite/polls/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.index, name = 'index'),
     # ex: /polls/
     url(r'^$', views.index, name='index'),
+    url('index2/', views.index2, name= 'index2'),
     # ex: /polls/5/
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
     # ex: /polls/5/results/
