b38fa41184d13a073e1c6911fca554ac84d75cee
yb123speed <904896209@qq.com>
2018-11-04 23:26:58 +0800

    添加404处理

--- Django/mysite/polls/urls.py
+++ Django/mysite/polls/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url('index2/', views.index2, name= 'index2'),
     # ex: /polls/5/
+    url(r'^detail2/(?P<question_id>[0-9]+)/$', views.detail2, name='detail2'),
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
     # ex: /polls/5/results/
     url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
--- Django/mysite/polls/views.py
+++ Django/mysite/polls/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
 from django.http import HttpResponse
 from django.template import loader
+from django.http import Http404
 from .models import Question
 
 def index(request):
@@ -19,7 +20,16 @@ def index2(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404('Question does not exist')
+    return render(request, 'polls/detail.html', {'question': question})
+
+def detail2(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
