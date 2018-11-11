774254e9106b87789ca809ad31ccca65b71f15f6
yb123speed <904896209@qq.com>
2018-11-11 20:35:21 +0800

    自定义ç®Question管理页面

--- Django/mysite/polls/admin.py
+++ Django/mysite/polls/admin.py
@@ -2,5 +2,20 @@ from django.contrib import admin
 from .models import Question, Choice
 
 # Register your models here.
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {
+            "fields": [
+                'question_text'
+            ],
+        }),
+        ('Date information', {
+            "fields": [
+                'pub_date'
+            ],
+        }),
+    ]
+    
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
