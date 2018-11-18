52038ca8d0cae8231713ace56e128ae9e07f745a
yb123speed <904896209@qq.com>
2018-11-18 23:47:15 +0800

    以表格形式展示â关联对象

--- Django/mysite/polls/admin.py
+++ Django/mysite/polls/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Question, Choice
 
 # Register your models here.
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 class QuestionAdmin(admin.ModelAdmin):
